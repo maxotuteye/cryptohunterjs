aab7bbb45a2cb4efedf817f6f03ac9def144e3c1
maxotuteye <maxotuteye12@gmail.com>
2021-10-06 13:27:21 +0000

    Added files for API calls to Coinranking

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import millify from 'millify';
 import {Typography, Row, Col, Statistic} from "antd";
+import {useGetCryptosQuery} from "../services/cryptoApi";
 
 const {Title} = Typography;
 
 const Homepage = () => {
+    const {data, isFetching} = useGetCryptosQuery();
+    console.log(data);
+
     return (
         <>
             <Title level={2} className="heading">
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,9 +1,24 @@
-/*
-var options = {
-    method: 'GET',
-    url: 'https://coinranking1.p.rapidapi.com/stats',
-    headers: {
-        'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-        'x-rapidapi-key': 'db6850b878msha050d6f8bace95fp10c512jsn73cd41f1e451'
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'db6850b878msha050d6f8bace95fp10c512jsn73cd41f1e451'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi(
+    {
+        reducerPath: 'cryptoApi',
+        baseQuery: fetchBaseQuery({baseUrl}),
+        endpoints: (builder) => ({
+            getCryptos: builder.query({
+                query: () => createRequest('/coins')
+            })
+        })
     }
-};*/
+)
+
+export const {useGetCryptosQuery} = cryptoApi;
