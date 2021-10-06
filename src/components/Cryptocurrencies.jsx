ee98ac620979ce603f96f9d5ca1ec290943f0697
maxotuteye <maxotuteye12@gmail.com>
2021-10-06 14:30:11 +0000

    Added functionality for Cryptocurrencies page

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
+import millify from 'millify'
+import {Link} from 'react-router-dom'
+import {Card, Row, Col, Input} from 'antd'
+import {useGetCryptosQuery} from "../services/cryptoApi";
 
 const Cryptocurrencies = () => {
+    const {data: cryptosList, isFetching} = useGetCryptosQuery()
+    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+
+    console.log(cryptos)
+
     return (
-        <div>
-            Cryptocurrencies
-        </div>
+        <>
+            <Row gutters={[32, 32]} className="crypto-card-container">
+                {cryptos.map((crypto) => (
+                    <Col xs={24} sm={12} lg={6} className="crypto-card" key={crypto.id}>
+                        <Link to={`/cryptos/${crypto.id}`}>
+                            <Card
+                                title={`${crypto.rank}. ${crypto.name}`}
+                                extra={<img className="crypto-image" src={crypto.iconUrl}/>}
+                                hoverable>
+
+                                <p>Price: {millify(crypto.price)}</p>
+                                <p>Market Cap: {millify(crypto.marketCap)}</p>
+                                <p>Daily Change: {millify(crypto.change)}%</p>
+
+                            </Card>
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
 
