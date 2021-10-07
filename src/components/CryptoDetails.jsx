ce15ca60927187d8d3ba7c18672e0be3aa367d8d
maxotuteye <maxotuteye12@gmail.com>
2021-10-07 14:06:49 +0000

    Added statistics and other info to cryptodetails page. Started crypto links section

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -86,8 +86,73 @@ const CryptoDetails = () => {
                 {time.map((period, i) => (
                     <Option value={period} key={period}>{period}</Option>
                 ))}
-
             </Select>
+
+            <Col className='stats-container'>
+                <Col className='coin-value-statistics'>
+                    <Col className='coin-value-statistics-heading'>
+                        <Title level={3} className='coin-details-heading'>
+                            {cryptoDetails.name} Value Statistics
+                        </Title>
+
+                        <p>
+                            An overview showing stats of {cryptoDetails.name}
+                        </p>
+
+                    </Col>
+
+                    {stats.map(({icon, title, value}) => (
+                        <Col className="coin-stats">
+                            <Col className='coin-stats-name'>
+                                <Text>{icon}</Text>
+                                <Text>{title}</Text>
+                            </Col>
+                            <Text className="stats">
+                                {value}
+                            </Text>
+                        </Col>
+                    ))}
+
+                </Col>
+
+                <Col className='other-stats-info'>
+                    <Col className='coin-value-statistics-heading'>
+                        <Title level={3} className='coin-details-heading'>
+                            Other Statistics
+                        </Title>
+                        {/* <p>
+                            Overview of the stats of all cryptocurrencies
+                        </p>*/}
+                    </Col>
+
+                    {genericStats.map(({icon, title, value}) => (
+                        <Col className="coin-stats">
+                            <Col className='coin-stats-name'>
+                                <Text>{icon}</Text>
+                                <Text>{title}</Text>
+                            </Col>
+                            <Text className="stats">
+                                {value}
+                            </Text>
+                        </Col>
+                    ))}
+
+                </Col>
+
+            </Col>
+            <Col className="coin-desc-link">
+                <Row className="coin-desc">
+                    <Title level={3} className='coin-details-heading'>
+                        What is {cryptoDetails.name}?
+                        {HTMLReactParser(cryptoDetails.description)}
+                    </Title>
+                </Row>
+                <Col className='coin-links'>
+                    <Title level={3} className='coin-details-heading'>
+                        {cryptoDetails.name} Links
+                    </Title>
+                </Col>
+            </Col>
         </Col>
     )
 }
