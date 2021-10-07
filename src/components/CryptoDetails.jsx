96324fd4c44605d1d661688a8c94bc80e7a12a6d
maxotuteye <maxotuteye12@gmail.com>
2021-10-07 18:07:48 +0000

    Finished crypto links. Began Line chart

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -151,6 +151,16 @@ const CryptoDetails = () => {
                     <Title level={3} className='coin-details-heading'>
                         {cryptoDetails.name} Links
                     </Title>
+                    {cryptoDetails.links.map((link) =>(
+                        <Row className='coin-link' key={link.name}>
+                            <Title level={5} className='link-name'>
+                                {link.type}
+                            </Title>
+                            <a href={link.url} target='_blank' rel='noreferrer'>
+                                {link.name}
+                            </a>
+                        </Row>
+                    ))}
                 </Col>
             </Col>
         </Col>
